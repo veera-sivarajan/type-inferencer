08a1aa5bb66505296109d3d1fa5ed425f1448c7d
Veera <sveera.2001@gmail.com>
2023-01-16 10:54:15 -0500

    Implement all helper functions for `extend_replace()`

--- src/inference.rs
+++ src/inference.rs
@@ -101,14 +101,56 @@ pub struct Substitution {
     is: Term,
 }
 
+fn left_occurs(rhs: &Term, left: &Term) -> bool {
+    match rhs {
+        Term::Arrow(ArrowType { domain, range }) => {
+            left_occurs(domain, left) || left_occurs(range, left)
+        }
+        _ => left == rhs,
+    }
+}
+
+fn replace(term: &Term, left: &Term, right: &Term) -> Term {
+    match term {
+        Term::Arrow(ArrowType { domain, range }) => Term::Arrow(ArrowType {
+            domain: Box::new(replace(domain, left, right)),
+            range: Box::new(replace(range, left, right)),
+        }),
+        _ => {
+            if term == left {
+                right.clone()
+            } else {
+                term.clone()
+            }
+        }
+    }
+}
+
+fn left_replace(subs: &mut [Substitution], left: &Term, right: &Term) -> Vec<Substitution> {
+    if subs.len() < 2 {
+        return vec![];
+    }
+    
+    let (first, rest) = subs.split_at(1);
+    let first = first[0];
+    
+    let var = first.var;
+    let is = replace(&first.is, left, right);
+    let mut result = vec![Substitution { var, is }];
+    result.extend(left_replace(&mut rest, left, right));
+    result
+}
+
 fn extend_replace(left: &Term, right: &Term, subst: Vec<Substitution>) -> Vec<Substitution> {
-    todo!()
+    
+
+
 }
 
 fn lookup(term: &Term, subs: &[Substitution]) -> Option<Term> {
     for s in subs {
         if s.var == *term {
-            return Some(s.var.clone());
+            return Some(s.is.clone());
         }
     }
     None
