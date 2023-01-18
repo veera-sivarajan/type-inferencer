218d778440651c293c6cc9d6910e497f3d277751
Veera <sveera.2001@gmail.com>
2023-01-18 16:17:34 -0500

    Intense debug mode

--- src/infer.rs
+++ src/infer.rs
@@ -17,7 +17,6 @@ impl Term {
     }
 }
 
-
 impl fmt::Display for Term {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -50,6 +49,9 @@ impl Types {
     pub fn infer(expr: &Expr) -> Substitutions {
         let mut state = Self::default();
         state.generate_constraints(expr);
+        for (k, v) in &state.constraints {
+            println!("{k} --> {v}");
+        }
         state.unify()
     }
 
@@ -117,13 +119,18 @@ impl Types {
     fn occurs_check(&self, left: &Term, right: &Term) -> bool {
         match left {
             Term::Arrow(ArrowType { domain, range }) => {
-                self.occurs_check(left, domain) || self.occurs_check(left, range)
+                self.occurs_check(left, domain)
+                    || self.occurs_check(left, range)
             }
             _ => left == right,
         }
     }
 
-    fn get_replacement(&self, left: Term, substitutions: &HashMap<Term, Term>) -> Option<Term> {
+    fn get_replacement(
+        &self,
+        left: Term,
+        substitutions: &HashMap<Term, Term>,
+    ) -> Option<Term> {
         for (k, v) in substitutions {
             if let Term::Arrow(func) = &k {
                 if *func.domain == left {
@@ -135,7 +142,6 @@ impl Types {
                 }
             }
 
-            
             if let Term::Arrow(func) = &v {
                 if *func.domain == left {
                     return Some(k.clone());
@@ -145,12 +151,24 @@ impl Types {
                     return Some(k.clone());
                 }
             }
-        } 
-        return Some(left);
+
+            if &left == k {
+                return Some(v.clone());
+            }
+
+            if &left == v {
+                return Some(k.clone());
+            }
+        }
+        Some(left)
     }
 
-
-    fn unify_helper(&self, left: &Term, right: &Term, substitutions: &mut Substitutions) {
+    fn unify_helper(
+        &self,
+        left: &Term,
+        right: &Term,
+        substitutions: &mut Substitutions,
+    ) {
         if !self.occurs_check(left, right) {
             if let Some(left) = self.get_replacement(left.clone(), substitutions) {
                 substitutions.insert(left, right.clone());
@@ -158,29 +176,74 @@ impl Types {
         }
     }
 
+    fn unify_assist(&self, left: &Term, right: &Term, substitutions: &mut Substitutions) {
+        if left == right {
+        } else if left.is_ident() {
+            self.unify_helper(left, right, substitutions);
+        } else if right.is_ident() {
+            self.unify_helper(right, left, substitutions);
+        } else {
+            match (left, right) {
+                (Term::Arrow(a_one), Term::Arrow(a_two)) => {
+                    let (d_one, d_two) =
+                        (a_one.domain.clone(), a_two.domain.clone());
+                    let (r_one, r_two) =
+                        (a_one.range.clone(), a_two.range.clone());
+                    self.unify_assist(
+                        &d_one,
+                        &d_two,
+                        substitutions,
+                    );
+                    self.unify_assist(
+                        &r_one,
+                        &r_two,
+                        substitutions,
+                    );
+                }
+                _ => {
+                    let msg =
+                        format!("{left} and {right} do not unify.");
+                    panic!("{msg}");
+                }
+            }
+        }
+    }
+
     fn unify(&mut self) -> Substitutions {
         let mut substitutions = HashMap::new();
         for (left, right) in &self.constraints {
-            if left == right {
-                continue;
-            } else if left.is_ident() {
-                self.unify_helper(left, right, &mut substitutions);
-            } else if right.is_ident() {
-                self.unify_helper(right, left, &mut substitutions);
-            } else {
-                match (left, right) {
-                    (Term::Arrow(a_one), Term::Arrow(a_two)) => {
-                        let (d_one, d_two) = (a_one.domain.clone(), a_two.domain.clone());
-                        let (r_one, r_two) = (a_one.range.clone(), a_two.range.clone());
-                        self.unify_helper(&d_one, &d_two, &mut substitutions);
-                        self.unify_helper(&r_one, &r_two, &mut substitutions);
-                    }
-                    _ => {
-                        let msg = format!("{left} and {right} do not unify.");
-                        panic!("{msg}");
-                    }
-                }
-            }
+            self.unify_assist(left, right, &mut substitutions);
+            // if left == right {
+            //     continue;
+            // } else if left.is_ident() {
+            //     self.unify_helper(left, right, &mut substitutions);
+            // } else if right.is_ident() {
+            //     self.unify_helper(right, left, &mut substitutions);
+            // } else {
+            //     match (left, right) {
+            //         (Term::Arrow(a_one), Term::Arrow(a_two)) => {
+            //             let (d_one, d_two) =
+            //                 (a_one.domain.clone(), a_two.domain.clone());
+            //             let (r_one, r_two) =
+            //                 (a_one.range.clone(), a_two.range.clone());
+            //             self.unify_helper(
+            //                 &d_one,
+            //                 &d_two,
+            //                 &mut substitutions,
+            //             );
+            //             self.unify_helper(
+            //                 &r_one,
+            //                 &r_two,
+            //                 &mut substitutions,
+            //             );
+            //         }
+            //         _ => {
+            //             let msg =
+            //                 format!("{left} and {right} do not unify.");
+            //             panic!("{msg}");
+            //         }
+            //     }
+            // }
         }
         return substitutions.clone();
     }
